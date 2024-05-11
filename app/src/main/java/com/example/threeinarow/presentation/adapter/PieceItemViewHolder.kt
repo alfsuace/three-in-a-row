3712120915445075e5c20cbfef5ff87ea10069d5
alfsuace <alfonsoxxi21@gmail.com>
2024-05-11 22:44:20 +0200

    feature 1: shared preferences presentation (wip)

---
+++ app/src/main/java/com/example/threeinarow/presentation/adapter/PieceItemViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.threeinarow.presentation.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PieceItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+    lateinit var
+
+}
