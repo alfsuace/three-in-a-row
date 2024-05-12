3db57d9fa661e772ff5116e2cbb60190910d8101
alfsuace <alfonsoxxi21@gmail.com>
2024-05-12 03:14:40 +0200

    feature 1: shared preferences viewmodel

--- app/src/main/java/com/example/threeinarow/presentation/PieceViewModel.kt
+++ app/src/main/java/com/example/threeinarow/presentation/PieceViewModel.kt
@@ -2,8 +2,10 @@ package com.example.threeinarow.presentation
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.threeinarow.domain.GetPiecesUseCase
 import com.example.threeinarow.domain.Piece
+import kotlinx.coroutines.Dispatchers
 
 class PieceViewModel(private val getPiecesUseCase: GetPiecesUseCase) {
 
@@ -11,8 +13,11 @@ class PieceViewModel(private val getPiecesUseCase: GetPiecesUseCase) {
     val uiState: LiveData<UiState> = _uiState
 
     fun loadPieces() {
-        _uiState.postValue(UiState())
-        viewModelScope
+        _uiState.postValue(UiState(pieces = emptyList()))
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = getPiecesUseCase.invoke()
+            _uiState.postValue(UiState(pieces = response))
+        }
     }
 
 
