3231eb1e5c95c0fc6b1093c93edac566f0935783
alfsuace <alfonsoxxi21@gmail.com>
2024-05-22 05:21:46 +0200

    feature 3: firebase: minnor fix

--- app/src/main/java/com/example/threeinarow/data/remote/PieceRemoteModel.kt
+++ app/src/main/java/com/example/threeinarow/data/remote/PieceRemoteModel.kt
@@ -4,7 +4,7 @@ import com.google.firebase.database.PropertyName
 
 data class PieceRemoteModel(
     @get:PropertyName("id") @set:PropertyName("id") var id: String,
-    @get:PropertyName("horizontal") @set:PropertyName("vertical") var horizontal: Int,
+    @get:PropertyName("horizontal") @set:PropertyName("horizontal") var horizontal: Int,
     @get:PropertyName("vertical") @set:PropertyName("vertical") var vertical: Int,
     @get:PropertyName("colour") @set:PropertyName("colour") var colour: String,
 
