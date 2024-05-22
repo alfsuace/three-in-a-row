5e47d9d6c87d3b6401fe41af8230c6f883d2c5fd
alfsuace <alfonsoxxi21@gmail.com>
2024-05-22 12:02:24 +0200

    feature 3: firebase: initialization of the database and internet permission on the manifest

--- app/src/main/java/com/example/threeinarow/MainActivity.kt
+++ app/src/main/java/com/example/threeinarow/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.threeinarow.presentation.PieceFragment
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        FirebaseApp.initializeApp(this)
         setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction()
             .replace(R.id.main, PieceFragment())
